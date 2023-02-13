5fae73a23292cfb4fb1f7adad0c8916325bd25ba
Amry <683348+ShamsulAmry@users.noreply.github.com>
2023-02-13 23:51:01 +0800

    LeetCode Problem 139 initial setup

---
+++ LeetCode/word-break/Solution.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// <see href="https://leetcode.com/problems/word-break/description/">
+///     LeetCode Problem 139: Word Break
+/// </see>
+/// </summary>
+public class Solution
+{
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
+        throw new NotImplementedException();
+    }
+}
