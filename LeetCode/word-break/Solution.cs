e2ba5b6ab2318c80388556e7e67a3d8f2cf5ac26
Amry <683348+ShamsulAmry@users.noreply.github.com>
2023-02-14 00:49:48 +0800

    LeetCode Problem 139 passes initial expectations
    but failed at Test Case 36 with Time Limit Exceeded error.

--- LeetCode/word-break/Solution.cs
+++ LeetCode/word-break/Solution.cs
@@ -6,7 +6,27 @@
 public class Solution
 {
     public bool WordBreak(string s, IList<string> wordDict)
+        => TestPhrase(s, wordDict, "");
+
+    bool TestPhrase(string s, IList<string> wordDict, string phrase)
     {
-        throw new NotImplementedException();
+        if (phrase.Length < s.Length) {
+            if (!s.StartsWith(phrase)) {
+                return false;
+            }
+        } else if (phrase.Length == s.Length) {
+            return phrase == s;
+        } else {
+            return false;
+        }
+
+        foreach (var word in wordDict) {
+            var longerPhrase = phrase + word;
+            if (TestPhrase(s, wordDict, longerPhrase)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
